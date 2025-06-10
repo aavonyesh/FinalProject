567998739cca065c828e87369ef0a714e7aa4dba
aavonyesh <aynurah-bf206@code.edu.az>
2025-06-10 23:33:56 +0400

    feat: add ExclusiveOffers section with styling and basic functionality

--- Frontend/client/src/pages/Home/Home.jsx
+++ Frontend/client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/section/Hero/Hero'
 import FeaturedDestination from '../../components/section/FeaturedDestination/FeaturedDestination'
+import ExclusiveOffers from '../../components/section/ExclusiveOffers/ExclusiveOffers'
 
 const Home = () => {
   return (
     <>
         <Hero/>
         <FeaturedDestination/>
+        <ExclusiveOffers/>
     </>
   )
 }
