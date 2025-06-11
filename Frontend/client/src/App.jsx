4a27a87293ac9849a92e528a9365b0d88a6f5927
aavonyesh <aynurah-bf206@code.edu.az>
2025-06-12 00:00:50 +0400

    feat: add Footer section with styling and basic functionality

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/GuestNavbar/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home/Home'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner")
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
+        <Footer/>
       </div> 
     </div>
   )
