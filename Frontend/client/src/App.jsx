b657ea97b15886106b5a6fa7ab5ac6a932a4baa2
aavonyesh <aynurah-bf206@code.edu.az>
2025-06-16 01:16:24 +0400

    feat(admin): add initial dashboard layout for admin panel

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -7,6 +7,10 @@ import AllRooms from './pages/AllRooms/AllRooms'
 import Detail from './pages/Detail/Detail'
 import MyBookings from './pages/MyBookings/MyBookings'
 import HotelReg from './components/HotelReg/HotelReg'
+import Layout from './pages/HotelOwner/Layout'
+import Dashboard from './pages/HotelOwner/Dashboard'
+import AddRoom from './pages/HotelOwner/AddRoom'
+import ListRoom from './pages/HotelOwner/ListRoom'
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner")
@@ -20,6 +24,11 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms/>}/>
           <Route path='/rooms/:id' element={<Detail/>}/>
           <Route path='/my-bookings' element={<MyBookings/>}/>
+          <Route path='/owner' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='add-room' element={<AddRoom/>}/>
+          <Route path='list-room' element={<ListRoom/>}/>
+          </Route>
         </Routes>
         <Footer/>
       </div> 
