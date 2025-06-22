a2580a5a05e85be93d209a3274b9ccc204d21606
aavonyesh <aynurah-bf206@code.edu.az>
2025-06-22 23:56:33 +0400

    chore(backend): setup backend base with folder structure and starter files

--- Frontend/client/src/pages/HotelOwner/ListRoom.jsx
+++ Frontend/client/src/pages/HotelOwner/ListRoom.jsx
@@ -31,30 +31,30 @@ const ListRoom = () => {
             </tr>
           </thead>
           <tbody className="text-sm">
-            {
-              rooms.map((item,index)=>(
-                <tr key={index}>
-                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
-                    {item.roomType}
-                  </td>
-                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden">
-                    {item.amenities.join(', ')}
-                  </td>
-                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
-                    {item.pricePerNight}
-                  </td>
-                  <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-sm text-red-500 text-center">
-                    <label className="relative inline-flex items-center cursor-pointer text-gray-9 gap-3">
-                      <input type="checkbox" className="sr-only peer" checked={item.isAvailable} />
-                      <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
-                      <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
-                    </label>
-                  </td>
-                  
-                </tr>
-              ))
-            }
-
+            {rooms.map((item, index) => (
+              <tr key={index}>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {item.roomType}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden">
+                  {item.amenities.join(", ")}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {item.pricePerNight}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-sm text-red-500 text-center">
+                  <label className="relative inline-flex items-center cursor-pointer text-gray-9 gap-3">
+                    <input
+                      type="checkbox"
+                      className="sr-only peer"
+                      checked={item.isAvailable}
+                    />
+                    <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
+                    <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
+                  </label>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
