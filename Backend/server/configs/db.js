be46a59e8350994427f307889bc78c911c46732a
aavonyesh <aynurah-bf206@code.edu.az>
2025-06-25 23:24:00 +0400

    Some changes

--- Backend/server/configs/db.js
+++ Backend/server/configs/db.js
@@ -1,15 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDb = async () => {
+const connectDB = async () => {
   try {
-    mongoose.connection.on('connected', () => {
-      console.log('Database Connected');
-    });
-
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
     await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
   } catch (error) {
     console.log(error.message);
   }
 };
 
-export default connectDb;
+export default connectDB;
